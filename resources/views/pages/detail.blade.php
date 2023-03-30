73f388aadd943c172e2c70b06f21c10f2dc26fe1
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-03-30 13:04:59 +0700

    3 Cart dan Provinsi

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -11,10 +11,12 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <nav aria-label="breadcrumb">
+                        <nav>
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="/">Home</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">
+                                <li class="breadcrumb-item">
+                                    <a href="/index.html">Home</a>
+                                </li>
+                                <li class="breadcrumb-item active">
                                     Product Details
                                 </li>
                             </ol>
@@ -23,12 +25,13 @@
                 </div>
             </div>
         </section>
-        <section class="store-gallery" id="gallery">
+
+        <section class="store-gallery mb-3" id="gallery">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8" data-aos="zoom-in">
                         <transition name="slide-fade" mode="out-in">
-                            <img :key="photos[activePhoto].id" :src="photos[activePhoto].url" class="w-100 main-image"
+                            <img :src="photos[activePhoto].url" :key="photos[activePhoto].id" class="w-100 main-image"
                                 alt="" />
                         </transition>
                     </div>
@@ -46,18 +49,30 @@
                 </div>
             </div>
         </section>
+
         <div class="store-details-container" data-aos="fade-up">
             <section class="store-heading">
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-8">
-                            <h1>Sofa Ternyaman</h1>
-                            <div class="owner">By Galih Pratama</div>
-                            <div class="price">$1,409</div>
+                            <h1>{{ $product->name }}</h1>
+                            <div class="owner">By {{ $product->user->store_name }}</div>
+                            <div class="price">${{ number_format($product->price) }}</div>
                         </div>
                         <div class="col-lg-2" data-aos="zoom-in">
-                            <a class="btn btn-success nav-link px-4 text-white btn-block mb-3" href="/cart.html">Add to
-                                Cart</a>
+                            @auth
+                                <form action="{{ route('detail-add', $product->id) }}" method="POST"
+                                    enctype="multipart/form-data">
+                                    @csrf
+                                    <button type="submit" class="btn btn-success px-4 text-white btn-block mb-3">
+                                        Add to Cart
+                                    </button>
+                                </form>
+                            @else
+                                <a href="{{ route('login') }}" class="btn btn-success px-4 text-white btn-block mb-3">
+                                    Sign in to Add
+                                </a>
+                            @endauth
                         </div>
                     </div>
                 </div>
@@ -66,20 +81,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-12 col-lg-8">
-                            <p>
-                                The Nike Air Max 720 SE goes bigger than ever before with
-                                Nike's tallest Air unit yet for unimaginable, all-day comfort.
-                                There's super breathable fabrics on the upper, while colours
-                                add a modern edge.
-                            </p>
-                            <p>
-                                Bring the past into the future with the Nike Air Max 2090, a
-                                bold look inspired by the DNA of the iconic Air Max 90.
-                                Brand-new Nike Air cushioning underfoot adds unparalleled
-                                comfort while transparent mesh and vibrantly coloured details
-                                on the upper are blended with timeless OG features for an
-                                edgy, modernised look.
-                            </p>
+                            {!! $product->description !!}
                         </div>
                     </div>
                 </div>
@@ -95,7 +97,7 @@
                         <div class="col-12 col-lg-8">
                             <ul class="list-unstyled">
                                 <li class="media">
-                                    <img src="/images/icon-testimonial-1.png" class="mr-3 rounded-circle" alt="" />
+                                    <img src="/images/icons-testimonial-1.png" alt="" class="mr-3 rounded-circle" />
                                     <div class="media-body">
                                         <h5 class="mt-2 mb-1">Hazza Risky</h5>
                                         I thought it was not good for living room. I really happy
@@ -103,8 +105,8 @@
                                         homey.
                                     </div>
                                 </li>
-                                <li class="media my-4">
-                                    <img src="/images/icon-testimonial-2.png" class="mr-3 rounded-circle" alt="" />
+                                <li class="media">
+                                    <img src="/images/icons-testimonial-2.png" alt="" class="mr-3 rounded-circle" />
                                     <div class="media-body">
                                         <h5 class="mt-2 mb-1">Anna Sukkirata</h5>
                                         Color is great with the minimalist concept. Even I thought
@@ -113,7 +115,7 @@
                                     </div>
                                 </li>
                                 <li class="media">
-                                    <img src="/images/icon-testimonial-3.png" class="mr-3 rounded-circle" alt="" />
+                                    <img src="/images/icons-testimonial-3.png" alt="" class="mr-3 rounded-circle" />
                                     <div class="media-body">
                                         <h5 class="mt-2 mb-1">Dakimu Wangi</h5>
                                         When I saw at first, it was really awesome to have with.
@@ -130,6 +132,8 @@
     </div>
 @endsection
 
+
+
 @push('addon-script')
     <script src="/vendor/vue/vue.js"></script>
     <script>
@@ -139,23 +143,14 @@
                 AOS.init();
             },
             data: {
-                activePhoto: 3,
-                photos: [{
-                        id: 1,
-                        url: "/images/product-details-1.jpg",
-                    },
-                    {
-                        id: 2,
-                        url: "/images/product-details-2.jpg",
-                    },
-                    {
-                        id: 3,
-                        url: "/images/product-details-3.jpg",
-                    },
-                    {
-                        id: 4,
-                        url: "/images/product-details-4.jpg",
-                    },
+                activePhoto: 0,
+                photos: [
+                    @foreach ($product->galleries as $gallery)
+                        {
+                            id: {{ $gallery->id }},
+                            url: "{{ Storage::url($gallery->photos) }}",
+                        },
+                    @endforeach
                 ],
             },
             methods: {
--- routes/web.php
+++ routes/web.php
@@ -2,42 +2,40 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 // Route::get('/debug-sentry', function () {
 //     throw new Exception('My first Sentry error!');
 // });
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/categories', 'CategoryController@index')->name('categories');
+Route::get('/categories/{id}', 'CategoryController@detail')->name('categories-detail');
+
 Route::get('/details/{id}', 'DetailController@index')->name('detail');
-Route::get('/cart', 'CartController@index')->name('cart');
+Route::post('/details/{id}', 'DetailController@add')->name('detail-add');
+
 Route::get('/success', 'CartController@success')->name('success');
 Route::get('/register/success', 'Auth\RegisterController@success')->name('register-success');
-Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
-Route::get('/dashboard/products', 'DashboardProductController@index')->name('dashboard-products');
-Route::get('/dashboard/products/create', 'DashboardProductController@create')->name('dashboard-products-create');
-Route::get('/dashboard/products/{id}', 'DashboardProductController@details')->name('dashboard-products-details');
 
-Route::get('/dashboard/transactions', 'DashboardTransactionController@index')->name('dashboard-transactions');
-Route::get('/dashboard/transactions/{id}', 'DashboardTransactionController@details')->name('dashboard-transactions-details');
+Route::group(['middleware' => ['auth']], function () {
+    // CART
+    Route::get('/cart', 'CartController@index')->name('cart');
+    Route::delete('/cart/{id}', 'CartController@delete')->name('cart-delete');
+    Route::post('/checkout', 'CheckoutController@process')->name('checkout');
 
-Route::get('/dashboard/settings', 'DashboardSettingController@store')->name('dashboard-settings-store');
-Route::get('/dashboard/account', 'DashboardSettingController@account')->name('dashboard-settings-account');
+    // PRODUCTS
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+    Route::get('/dashboard/products', 'DashboardProductController@index')->name('dashboard-products');
+    Route::get('/dashboard/products/create', 'DashboardProductController@create')->name('dashboard-products-create');
+    Route::get('/dashboard/products/{id}', 'DashboardProductController@details')->name('dashboard-products-details');
 
-//
-Route::get('/categories/{id}', 'CategoryController@detail')->name('categories-detail');
-Route::get('/details/{id}', 'DetailController@index')->name('detail');
+    // TRANSACTION
+    Route::get('/dashboard/transactions', 'DashboardTransactionController@index')->name('dashboard-transactions');
+    Route::get('/dashboard/transactions/{id}', 'DashboardTransactionController@details')->name('dashboard-transactions-details');
+
+    // SETTINGS
+    Route::get('/dashboard/settings', 'DashboardSettingController@store')->name('dashboard-settings-store');
+    Route::get('/dashboard/account', 'DashboardSettingController@account')->name('dashboard-settings-account');
+});
 
 Route::prefix('admin')
     ->namespace('Admin')
